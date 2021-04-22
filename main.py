f7609b7ebd74418497864eb1c07370db30dea073
Yash Singh <32360914+yashsinghcodes@users.noreply.github.com>
2021-04-22 17:49:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 import urllib.request
+import requests
 import socket
 try:
     from urllib.parse import urlparse
@@ -8,39 +9,42 @@ except ImportError:
 import json
 import sys,os
 
-def location(IP):
-    response = urllib.request.urlopen("https://geolocation-db.com/json/"+str(IP[len(IP)-2]))
-    encode = response.info().get_content_charset('utf8')
-    data = json.loads(response.read().decode(encode))
+def location(li):
+#    response = urllib.request.urlopen("https://geolocation-db.com/json/"+str(IP[len(IP)-2]))
+#    encode = response.info().get_content_charset('utf8')
+#    data = json.loads(response.read().decode(encode))
+    r = requests.get("http://geolocation-db.com/json/"+str(li))
+    data = r.json()
     try:
         lat = float(data['latitude'])
         lon = float(data['longitude'])
         country = str(data['country_name'])
         if lat == 0.0 and log == 0.0:
             print('None')
-            return(None,None)
+            return(None,None,None)
         return (lat,lon,country)
     except Exception as e:
-        return(None,None)
+        return(None,None,None)
                 
 def geoloc(url):
-    url1 = 'https://' + url
+    url1 = 'http://' + url
     url1 = urlparse(url1)
     url1 = url1.netloc
     com = Popen(['tracert', url], stdout=PIPE)
-    latar = []
-    logar = []
     print('IP        Country        Latitude        Longitude')
     while True:
         line = com.stdout.readline()
         line2 = str(line).replace('\\r','').replace('\\n','')
         IP = line2.split(' ')
-        if '.' in IP[len(IP)-2] and IP[len(IP)-2] != url:
-            lat,log,country = location(IP[len(IP)-2])
-            if lat:
-                print(str(IP[len(IP)-2])+'        '+str(country)+'        '+str(lat)+'        '+str(log))
-        if not line:
-            break
+        try:
+            if '.' in IP[len(IP)-2] and IP[len(IP)-2] != url:
+                lat,log,country = location(IP[len(IP)-2])
+                if lat:
+                    print(str(IP[len(IP)-2])+'        '+str(country)+'        '+str(lat)+'        '+str(log))
+            if not line:
+                break
+        except Exception as e:
+            print('Error')
 
 link = sys.argv[1]
 print(geoloc(str(link)))
